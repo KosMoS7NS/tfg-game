6b641916757d38a8fa4473b051bfaa819cd6e4a5
KosMoS7NS <danieldmiguel2019@gmail.com>
2022-05-18 18:42:16 +0200

    Bugs fixed
    Updated project

---
+++ TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/GestorEscenas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestorEscenas : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Scene_A");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("DevsMenu");
+    }
+
+    public void IrMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
