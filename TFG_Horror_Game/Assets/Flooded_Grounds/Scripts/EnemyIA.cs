73abc22f8f6b36e5a52a48ffb5c730e43d041b1e
Rubén Palomo Fontán <99551384+RubenPalomo@users.noreply.github.com>
2022-06-15 20:22:18 +0200

    Changes in the enemy movements
    I'm bilingual and you know it

--- TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/EnemyIA.cs
+++ TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/EnemyIA.cs
@@ -15,23 +15,32 @@ public class EnemyIA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        vector = new Vector3(0.1f, 0, 0);
+        vector = new Vector3(-0.3f, 0, 0);
         animator = GetComponent<Animator>();
+        InvokeRepeating("ComportamientoEnemigo", 1, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        ComportamientoEnemigo();
+    }
+
+    public void AvanceEnemigo()
+    {
+        transform.Translate(vector * 10 * Time.deltaTime);
     }
 
     public void ComportamientoEnemigo()
     {
+        try
+        {
+            CancelInvoke("AvanceEnemigo");
+        }
+        catch { }
         cronometro += Random.Range(-2, 2)* 100 * Time.deltaTime;
         rutina = (int)cronometro;
         if (cronometro >= 4 || cronometro < 0)
             cronometro = 0;
-        Debug.Log(Time.deltaTime);
         switch (rutina)
         {
             // Stop enemy
@@ -48,7 +57,7 @@ public class EnemyIA : MonoBehaviour
             // Walk enemy
             default:
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(vector * 2 * Time.deltaTime);
+                InvokeRepeating("AvanceEnemigo", 0, 0);
                 animator.SetBool("walk", true);
                 break;
         }
