dd2fb3f46c0e5d25108612dea5b58dffd7dd358f
KosMoS7NS <104136933+KosMoS7NS@users.noreply.github.com>
2022-09-17 13:03:45 +0200

    Linterna
    Recogida de linterna terminada

--- TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/FlashLightPick.cs
+++ TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/FlashLightPick.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class FlashLightPick : MonoBehaviour
 {
     public Inventory inventory;
+    public GameObject linterna;
+   // Vector3 vector = new Vector3(0.15, -0.35, 0.6);
+
     void Start()
     {
+        linterna.SetActive(false);
+        this.gameObject.SetActive(true);
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
 
     }
@@ -14,9 +19,11 @@ public class FlashLightPick : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") {
-
+            linterna.SetActive(true);
+            // var postion = gameObject.GetComponent<Transform>().position;
+            // postion = new Vector3(0, 4, 8);
             inventory.cantidad += 1;
-            Destroy(gameObject);
+            this.gameObject.SetActive(false);
         }
     }
 }
