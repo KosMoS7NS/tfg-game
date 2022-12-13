0fbafecc4820bdc483ba8898aaf4a14159dddf6d
KosMoS7NS <104136933+KosMoS7NS@users.noreply.github.com>
2022-12-13 19:32:36 +0100

    Project finished v1.0

--- TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/FlashLightPick.cs
+++ TFG_Horror_Game/Assets/Flooded_Grounds/Scripts/FlashLightPick.cs
@@ -7,6 +7,8 @@ public class FlashLightPick : MonoBehaviour
     [SerializeField] private AudioClip m_TakeObjSound;
     public Inventory inventory;
     public GameObject linterna;
+    public GameObject arma;
+    public GameObject weapon;
     public GameObject bateriesPanel;
     private BateryController bateryController;
     private AudioSource m_AudioSource;
@@ -16,6 +18,7 @@ public class FlashLightPick : MonoBehaviour
         m_AudioSource = GetComponent<AudioSource>();
         bateryController = FindObjectOfType<BateryController>();
         linterna.SetActive(false);
+        arma.SetActive(false);
         this.gameObject.SetActive(true);
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
         bateriesPanel.SetActive(false);
@@ -33,8 +36,10 @@ public class FlashLightPick : MonoBehaviour
             m_AudioSource.Play();
             bateryController.AddBatery();
             linterna.SetActive(true);
+            arma.SetActive(true);
             inventory.cantidad += 1;
             this.gameObject.SetActive(false);
+            weapon.SetActive(false);
             bateriesPanel.SetActive(true);
             InvokeRepeating("RemoveEnergy", 5, 5);
         }
